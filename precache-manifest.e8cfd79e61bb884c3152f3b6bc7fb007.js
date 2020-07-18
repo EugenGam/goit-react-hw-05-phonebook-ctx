4381228ebe98a2a40b22323262469cdf31accd8e
Анастасия <eugengamilov@gmail.com>
2020-07-18 21:44:43 +0300

    Updates

---
+++ precache-manifest.e8cfd79e61bb884c3152f3b6bc7fb007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e4a58310b92d251e6c8f7c20fa3b95",
+    "url": "/goit-react-hw-05-phonebook-ctx/index.html"
+  },
+  {
+    "revision": "0122c854c65c6dd20e43",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/css/main.2ba146cc.chunk.css"
+  },
+  {
+    "revision": "a8e00c4593f29f224d03",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.18db8269.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.18db8269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0122c854c65c6dd20e43",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.5762f9b9.chunk.js"
+  },
+  {
+    "revision": "c086547a9f9469ca633a",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/runtime-main.a7bebd28.js"
+  }
+]);
